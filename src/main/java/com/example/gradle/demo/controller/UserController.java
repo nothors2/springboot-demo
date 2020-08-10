14eb0066ce41488ebedf01eac3a0b10010531e84
James Song <nothorse@gmail.com>
2020-08-11 00:08:41 +0900

    swagger 추가

--- src/main/java/com/example/gradle/demo/controller/UserController.java
+++ src/main/java/com/example/gradle/demo/controller/UserController.java
@@ -1,33 +1,38 @@
 package com.example.gradle.demo.controller;
+ 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 import com.example.gradle.demo.entity.User;
 import com.example.gradle.demo.repo.UserJpaRepo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import lombok.RequiredArgsConstructor;
+@Api(tags = {"1. User"})
 @RequiredArgsConstructor
-@RestController // 결과값을 JSON으로 출력합니다.
+@RestController
 @RequestMapping(value = "/v1")
 public class UserController {
-  private final UserJpaRepo userJpaRepo;
 
-  @GetMapping(value = "/user")
-  public List<User> findAllUser() {
-      return userJpaRepo.findAll();
-  }
+    private final UserJpaRepo userJpaRepo;
 
-  @PostMapping(value = "/user")
-  public User save() {
-      User user = User.builder()
-              .uid("yumi12@naver.com")
-              .name("유미")
-              .build();
-      return userJpaRepo.save(user);
-  }  
+    @ApiOperation(value = "회원 조회", notes = "모든 회원을 조회한다")
+    @GetMapping(value = "/user")
+    public List<User> findAllUser() {
+        return userJpaRepo.findAll();
+    }
+
+    @ApiOperation(value = "회원 입력", notes = "회원을 입력한다.")
+    @PostMapping(value = "/user")
+    public User save(@ApiParam(value = "회원아이디", required = true) @RequestParam String uid,
+                     @ApiParam(value = "회원이름", required = true) @RequestParam String name) {
+        User user = User.builder()
+                .uid(uid)
+                .name(name)
+                .build();
+        return userJpaRepo.save(user);
+    }
 }
